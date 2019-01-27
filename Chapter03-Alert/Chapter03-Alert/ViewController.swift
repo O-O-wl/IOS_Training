8edc1da45f541b058e20867fad47eb337080bdc9
dongY <ldcpaul@hanmail.net>
2019-01-27 16:44:03 +0900

    커스터마이징 알림창 MapView

--- Chapter03-Alert/Chapter03-Alert/ViewController.swift
+++ Chapter03-Alert/Chapter03-Alert/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import MapKit
 class ViewController: UIViewController{
 
     var defaultButton : UIButton?
@@ -20,7 +20,7 @@ class ViewController: UIViewController{
        defaultButton?.setTitle("알림창", for: .normal)
         
         
-        defaultButton?.addTarget(self, action: #selector(popAlert(sender:))
+        defaultButton?.addTarget(self, action: #selector(popMapAlert(sender:))
             , for: .touchUpInside)
         self.view.addSubview(defaultButton!)
        
@@ -39,6 +39,33 @@ class ViewController: UIViewController{
         alert.setValue(contentView, forKey: "contentViewController")
         alert.addAction(okAction)
         alert.addAction(cancelAction)
+        self.present(alert, animated: false)
+    }
+    
+    @objc func popMapAlert(sender:Any){
+        let alert = UIAlertController(title: nil, message: "여기에 계신가요?", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        let mapViewController  = UIViewController()
+        let map = MKMapView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+    
+        let location = CLLocationCoordinate2D(latitude: 37.487432, longitude: 126.825966)
+        let distance : CLLocationDistance = 200
+        
+        let region  = MKCoordinateRegion.init(center: location, latitudinalMeters: distance, longitudinalMeters: distance)
+        let pin = MKPointAnnotation()
+        
+        pin.coordinate = location
+        pin.title = "성공회대학교"
+        map.region = region
+        
+        map.addAnnotation(pin)
+        
+        mapViewController.view = map
+        alert.setValue(mapViewController, forKey: "contentViewController")
         self.present(alert, animated: false)
     }
 }
