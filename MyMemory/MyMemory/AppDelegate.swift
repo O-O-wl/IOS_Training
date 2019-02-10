258abd6789bc975e2ebeb1ee72dead6b0254b8e9
dongY <ldcpaul@hanmail.net>
2019-02-10 15:56:07 +0900

    슬라이싱 처리를 위한 이미지에셋 세트 추가 , -> 앱씨닝

--- MyMemory/MyMemory/AppDelegate.swift
+++ MyMemory/MyMemory/AppDelegate.swift
@@ -41,10 +41,51 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
+
+    /**=============================================================
+     - Note:     앱이 종료될때 호출
+     ===============================================================**/
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
+        self.saveContext()
     }
-
+    
+    
+    
+    // - MARK: - 코어데이터 반입시 추가 구현부
+    /**=============================================================
+     - Note:     앱이 종료될때 호출
+     ===============================================================**/
+    
+    lazy var persistentContainer : NSPersistentContainer = {
+        
+        /// - Note: 영구 컨테이너객체 생성 -- 'DataModel'파일을 바탕으로 생성
+        let container = NSPersistentContainer(name: "MyMemory")
+        
+        /// - Note: 영구 저장소 정보를 읽어서 변수 초기화
+        container.loadPersistentStores{
+            if let error = $1 as NSError?{
+                fatalError("Unsolved error \(error), \(error.userInfo) ")
+            }
+        }
+        
+        return container
+    }()
+    
+    
+    func saveContext(){
+        let context = persistentContainer.viewContext
+        
+        if context.hasChanges{
+            do{
+                try context.save()
+                
+            }catch let error as NSError{
+                fatalError("Unsolved error \(error), \(error.userInfo) ")
+            }
+        }
+    }
 
 }
 
